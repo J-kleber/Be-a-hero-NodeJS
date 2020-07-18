bcc985d413a65e8bc6064e46e1167f945a8d221e
unknown <john@emporioht.com.br>
2020-07-18 16:28:42 -0300

    commit inicial

---
+++ src/routes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OngController.index);
+
+routes.post('/ongs', celebrate({date
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngController.create);
+
+
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+}), ProfileController.index);
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), IncidentController.index);
+
+routes.post('/incident', IncidentController.create);
+
+routes.delete('/incident/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), IncidentController.destroy);
+
+module.exports = routes; 
